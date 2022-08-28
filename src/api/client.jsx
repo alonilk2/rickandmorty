e4f7c1057024156e3f926eefeab4680837fbf036
Alon Barenboim <alonilk2@gmail.com>
2022-08-28 22:15:58 +0300

    final push

--- src/api/client.jsx
+++ src/api/client.jsx
@@ -1,3 +1,4 @@
 import axios from "axios";
+import { base_api_url } from "../config/config";
 
-export default axios.create({ baseURL: "https://rickandmortyapi.com/api" });
+export default axios.create({ baseURL: base_api_url });
